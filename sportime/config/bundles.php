3469fd7b6fdc837e0bee9eceb4b6041c473f6ca3
Adrian Tagirta <alexmola917@gmail.com>
2023-06-09 11:12:48 +0200

    Swagger OK

--- sportime/config/bundles.php
+++ sportime/config/bundles.php
@@ -10,4 +10,7 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
